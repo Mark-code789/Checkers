e4035abb4ca3d9435a42779df00e669746562a1f
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-08-31 15:36:35 +0300

    update Channel.js

--- Channel.js
+++ Channel.js
@@ -427,7 +427,6 @@ class Publish {
         let self = this;
 		Lobby.PUBNUB.publish(config, async (status, response) => {
             if(!status.error) {
-            	alert(JSON.parse(config.message).title);
                 if(JSON.parse(config.message).title === "IntentionalExit") {
 					self.retryCount = 0;
         			self.messages = [];
