1e226525f7ce9f6c4b1f9009ee23bbdd82af8e1d
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-09-20 20:01:44 +0300

    update Service Worker.js

--- Service Worker.js
+++ Service Worker.js
@@ -1,5 +1,5 @@
 // Service worker
-const cacheName = "Checkers v6.5.2";
+const cacheName = "Checkers v6.5.3";
 const appShellFiles = [
     "./src/images/american flag.jpeg",
     "./src/images/kenyan flag.jpeg",
