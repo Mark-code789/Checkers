008f7341a14b4d7b87a5f448a77273d902ff9edb
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-11-09 17:45:04 +0300

    update Service Worker.js

--- Service Worker.js
+++ Service Worker.js
@@ -58,7 +58,7 @@ self.addEventListener("install", (e) => {
 
 self.addEventListener("fetch", (e) => {
     e.respondWith(
-        caches.match(e.request).then((res1) => {
+        caches.match(e.request, {ignoreSearch: true}).then((res1) => {
             if(navigator.onLine && /(?<!min).(html|css|js)(.*?)$/g.test(e.request.url) || !res1) {
                 return fetch(e.request).then((res2) => {
                     return caches.open(cacheName).then((cache) => {
