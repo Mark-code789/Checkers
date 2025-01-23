77d2c578ff4aa4359d30e458f9c8f6bc9c1fc8ef
Mark Etale <etalemark@gmail.com>
2025-01-23 19:41:53 +0300

    Update move checker.js

--- src/scripts/ui/move checker.js
+++ src/scripts/ui/move checker.js
@@ -150,7 +150,7 @@ class MoveChecker {
 			let prev = this.selected;
 			this.selected = this.capturePaths.reduce((found, path) => {
 				let n = -1; 
-				let p = this.clicked.values().every(({i, j}) => {
+				let p = [...this.clicked.values()].every(({i, j}) => {
 					let m = path.findIndex((move) => move.getToRow() == i && move.getToCol() == j);
 						
 					if(Boolean(~m) && m > n) {
