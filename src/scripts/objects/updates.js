78b724fe41ea21b26a3ef85c487336257d4d82ef
Mark Etale <etalemark@gmail.com>
2025-01-23 04:45:53 +0300

    Add files via upload

--- src/scripts/objects/updates.js
+++ src/scripts/objects/updates.js
@@ -1,6 +1,6 @@
-class Updates {
-	static version = "30.21.218.503";
-	static updatesLog = new Map([
+window.Updates = {
+	version: "30.21.218.559", 
+	updatesLog: new Map([
 		["22.15.200.529", ["Added voice notes in the chat engine.", "Added delete and copy option for chat engine.", "Improved internal operations.", "Improved the AI thinking time.", "Fixed channel subscription error.", "Fixed more other errors."]], 
 		["22.15.201.530", ["Fixed single player draw issue.", "Fixed fullscreen not changing orientation.", "Fixed game stats behind by a move.", "Added locking orientation in both primary and secondary."]], 
 		["22.15.204.536", ["Fixed minor bags."]], 
@@ -12,13 +12,13 @@ class Updates {
 		["23.16.211.543", ["Improved internal operations."]], 
 		["23.16.211.545", ["Fixed offline loading error."]], 
 		["24.17.214.550", ["Fixed refresh error.", "Improved game difficulty.", "Fixed minor bugs."]], 
-		["30.21.218.503", ["Added 2 new levels per version.", "Improved game performance.", "Improved game difficulty", "Change online match connectivity", "Fixed interface breaking on different size screens", "Added other features, discover by yourself"]], 
-	]);
-	static getDescription = (version) => {
+		["30.21.218.559", ["Added 2 new levels per version.", "Improved game performance.", "Improved game difficulty", "Change online match connectivity", "Fixed interface breaking on different size screens", "Added other features, discover by yourself"]], 
+	]), 
+	getDescription: function (version) {
 		let versionDescription = "<ul>";
 		if(!version) {
 			for(let [key, value] of this.updatesLog.entries()) {
-				if(key >= currentAppVersion) {
+				if(key >= this.version) {
 					versionDescription += `<li>Version: ${key}</li><ul>${value.map(desc => "<li>" + desc + "</li>").join("")}</ul>`;
 				} 
 			} 
@@ -31,4 +31,4 @@ class Updates {
 		versionDescription += "</ul>";
 		return versionDescription;
 	} 
-} 
+}
