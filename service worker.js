74a3c5893ed7b37a0b0015ee83f8750653044996
Mark Etale <etalemark@gmail.com>
2025-01-26 09:58:10 +0300

    update service worker.js

--- service worker.js
+++ service worker.js
@@ -139,7 +139,7 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
     e.respondWith(
         caches.match(e.request.url.replace(/Checkers\/$/i, t => t + "index.html"), {cacheName, ignoreSearch: true}).then( async (res) => {
-        	if(res && !/updates.js$/gi.test(e.request.url)) {
+        	if(res && !/(updates.js|\.css)$/gi.test(e.request.url)) {
             	return res;
             }
             
